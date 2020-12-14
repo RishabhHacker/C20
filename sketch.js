3156ab3f0615e35f44525bd0f914b5d910ce9416
RishabhHacker <73529254+RishabhHacker@users.noreply.github.com>
2020-12-14 21:30:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,35 @@
-var car,wall;
-var speed,weight;
+var fixedRect, movinRect;
 function setup() {
-  createCanvas(1500,400);
-  createSprite(400, 200, 50, 50);
-  speed=random(55,98)
-  weight=random(400,1500)
-
+  createCanvas(1200,800);
+ movingRect= createSprite(400, 200, 50, 50);
+ fixedRect=createSprite(600,400,70,50)
+ movingRect.shapeColor='red';
+ fixedRect.shapeColor='red';
 }
 
 function draw() {
-  background(255,255,255);  
-  car=createSprite(50,200,50,50)
-  wall=createSprite(1500,200,60,height/2)
-car.velocityX=speed
+  background(0);
+  movingRect.x=World.mouseX ;
+  movingRect.y=World.mouseY;
+  //console.log(movingRect.x-fixedRect.x) 
+  console.log(movingRect.width/2+fixedRect.width/2)
+  if(movingRect.x-fixedRect.x<movingRect.width/2+
+    fixedRect.width/2
+     &&
+     fixedRect.x-movingRect.x<movingRect.width/2+
+    fixedRect.width/2
+    &&
+     movingRect.y-fixedRect.y<movingRect.width/2+
+      fixedRect.width/2
+       &&
+       fixedRect.y-movingRect.y<movingRect.width/2+
+      fixedRect.width/2)
+    {
+      movingRect.shapeColor='green'
+      console.log(movingRect.width/2+
+        fixedRect.width/2)
+    }
+    else{movingRect.shapeColor='red'}
+    
   drawSprites();
 }
